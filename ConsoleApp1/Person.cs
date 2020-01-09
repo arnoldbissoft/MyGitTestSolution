dbd54bc613849d70eedd05fbc0b8c114ad4ddeee
Arnold <arnoldk@bissoft.ro>
2020-01-09 12:37:51 +0200

    Adding Person

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
